b446a93d40086ae88f49e8bdf5f9850c428257ce
Dakodah Bradley <comatose9592@gmail.com>
2022-05-27 17:33:25 -0500

    final commit

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 const key = "a9f93c8db640f8d2581145d2580c7040"
 
 // Get token from Open Weather
-const userWeather = async () => {
+const userWeather = async (e) => {
     // Making a fetch request
-    const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${key}&units=imperial`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+    e.preventDefault();
+    let city = e.target.city.value
+    const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`, {
+        method: 'GET',
     })
     const data = await response.json()
-    document.getElementById('output').innerHTML = data
+    console.log(data)
+
+    document.getElementById('output_high').innerHTML = data.main.temp_max
+    document.getElementById('output_low').innerHTML = data.main.temp_min
+    document.getElementById('output_forecast').innerHTML = data.weather[0].main
+    document.getElementById('output_humidity').innerHTML = data.main.humidity
     
 };
 
-document.getElementById('search').addEventListener('click', userWeather)
+document.getElementById('search').addEventListener('submit', userWeather)
+
+
+
